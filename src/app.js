e6ca1d05ae7b395fcea07c022ae999b8683865c3
JoseGeek78 <126101249+JoseGeek78@users.noreply.github.com>
2023-10-07 08:53:54 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,20 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  document.querySelector("#excuse").innerHTML = generateExcuse();
-  console.log("My dog ate my homework!");
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = createExcuse();
 };
-let generateExcuse = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
 
-  const whoIndex = Math.floor(Math.random() * who.length);
-  const actionIndex = Math.floor(Math.random() * action.length);
-  const whatIndex = Math.floor(Math.random() * what.length);
-  const whenIndex = Math.floor(Math.random() * when.length);
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying"];
 
-  const excuse = `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}.`;
+function getElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
-  return excuse;
-};
+// Cambiamos createExcuse de una cadena a una función
+function createExcuse() {
+  return `${getElement(who)} ${getElement(action)} ${getElement(what)} ${getElement(when)}`;
+}
